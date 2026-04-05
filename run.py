ebb83066fa273b6284201659931f0f63a85962b3
mohammednabeel123 <abdulmuminnabeelmohammed@gmail.com>
2026-04-05 11:51:13 +0300

    edits

--- run.py
+++ run.py
@@ -6,9 +6,72 @@ app = Flask(
     static_folder="app/static"
 )
 
+products = [
+    {
+        "id": 1,
+        "name": "Black Oversized T-Shirt",
+        "price": 29,
+        "category": "men",
+        "images": [
+            "images/image1.jpg",
+            "images/image1_same.jpg"
+        ]
+    },
+    {
+        "id": 2,
+        "name": "White Hoodie",
+        "price": 59,
+        "category": "men",
+        "images": [
+            "images/image2.jpg",
+            "images/image2_same.jpg"
+        ]
+    },
+    {
+        "id": 2,
+        "name": "White Hoodie",
+        "price": 59,
+        "category": "men",
+        "images": [
+            "images/image3.jpg",
+            "images/image3_same.jpg"
+        ]
+    },
+    {
+        "id": 2,
+        "name": "White Hoodie",
+        "price": 59,
+        "category": "men",
+        "images": [
+            "images/image2.jpg",
+            "images/image2_same.jpg"
+        ]
+    },
+    {
+        "id": 2,
+        "name": "White Hoodie",
+        "price": 59,
+        "category": "men",
+        "images": [
+            "images/image2.jpg",
+            "images/image2_same.jpg"
+        ]
+    },
+]
+
 @app.route("/")
 def home():
-    return render_template("main/index.html")
+    featured_products = products[:4]
+    return render_template("main/index.html", products=featured_products)
+
+@app.route("/shop")
+def shop():
+    return render_template("shop/shop.html", products=products)
+
+@app.route("/shop/<category>")
+def shop_category(category):
+    filtered_products = [p for p in products if p["category"] == category]
+    return render_template("shop/shop.html", products=filtered_products, category=category)
 
 if __name__ == "__main__":
     app.run(debug=True)
